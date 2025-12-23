390640dd62a3795a7abbf2af1f47f79fd3232f35
twostars <i.am.twostars@gmail.com>
2025-12-24 08:03:42 +1100

    Update deps/openal-soft-msvc/version.h

--- deps/openal-soft-msvc/version.h
+++ deps/openal-soft-msvc/version.h
@@ -1,9 +1,9 @@
 /* Define to the library version */
-#define ALSOFT_VERSION "1.24.3"
-#define ALSOFT_VERSION_NUM 1,24,3,0
+#define ALSOFT_VERSION "1.25.0"
+#define ALSOFT_VERSION_NUM 1,25,0,0
 
 /* Define the branch being built */
 #define ALSOFT_GIT_BRANCH "master"
 
 /* Define the hash of the head commit */
-#define ALSOFT_GIT_COMMIT_HASH "bb2c41323"
+#define ALSOFT_GIT_COMMIT_HASH "d5f84aa0a"
