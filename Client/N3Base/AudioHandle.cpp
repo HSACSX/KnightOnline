6585d2104b9687a5101fb39fa603d663aafe7c2a
twostars <i.am.twostars@gmail.com>
2025-12-28 17:01:34 +1100

    Move mpg123 reader I/O callbacks to their won file

--- Client/N3Base/AudioHandle.cpp
+++ Client/N3Base/AudioHandle.cpp
@@ -4,13 +4,9 @@
 #include "N3SndMgr.h"
 #include "AudioAsset.h"
 #include "al_wrapper.h"
+#include "mpg123_reader_io.h"
 
 #include <FileIO/FileReader.h>
-#include <mpg123.h>
-
-mpg123_ssize_t mpg123_filereader_read(void* userData, void* dst, size_t bytes);
-off_t mpg123_filereader_seek(void* userData, off_t offset, int whence);
-void mpg123_filereader_cleanup(void* userData);
 
 AudioHandle::AudioHandle()
 {
@@ -242,64 +238,4 @@ StreamedAudioHandle::~StreamedAudioHandle()
 		mpg123_delete(Mp3Handle);
 		Mp3Handle = nullptr;
 	}
-}
-
-mpg123_ssize_t mpg123_filereader_read(void* userData, void* dst, size_t bytes)
-{
-	if (bytes == 0)
-		return 0;
-
-	FileReaderHandle* handle = static_cast<FileReaderHandle*>(userData);
-	const FileReader* file = handle->File;
-	const size_t size = static_cast<size_t>(file->Size());
-
-	if (handle->Offset >= size)
-		return 0;
-
-	const size_t remainingBytes = size - handle->Offset;
-	size_t bytesToRead = std::min(bytes, remainingBytes);
-
-	std::memcpy(
-		dst,
-		static_cast<const uint8_t*>(file->Memory()) + handle->Offset,
-		bytesToRead);
-
-	handle->Offset += bytesToRead;
-	return static_cast<mpg123_ssize_t>(bytesToRead);
-}
-
-off_t mpg123_filereader_seek(void* userData, off_t offset, int whence)
-{
-	FileReaderHandle* handle = static_cast<FileReaderHandle*>(userData);
-	const FileReader* file = handle->File;
-	const size_t size = static_cast<size_t>(file->Size());
-
-	size_t newPos = 0;
-
-	switch (whence)
-	{
-		case SEEK_SET:
-			newPos = static_cast<size_t>(offset);
-			break;
-
-		case SEEK_CUR:
-			newPos = handle->Offset + static_cast<size_t>(offset);
-			break;
-
-		case SEEK_END:
-			newPos = size + static_cast<size_t>(offset);
-			break;
-
-		default:
-			errno = EINVAL;
-			return -1; // unhandled type
-	}
-
-	handle->Offset = newPos;
-	return static_cast<off_t>(handle->Offset);
-}
-
-void mpg123_filereader_cleanup(void* /*userData*/)
-{
-	/* do nothing, cleanup is managed externally */
 }
---
+++ Client/N3Base/mpg123_reader_io.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include <mpg123.h>
+
+mpg123_ssize_t mpg123_filereader_read(void* userData, void* dst, size_t bytes);
+off_t mpg123_filereader_seek(void* userData, off_t offset, int whence);
+void mpg123_filereader_cleanup(void* userData);
