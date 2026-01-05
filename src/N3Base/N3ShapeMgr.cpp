aa7e8ed08d21b3e815d759c7005807bc83687135
twostars <i.am.twostars@gmail.com>
2026-01-06 00:24:22 +1100

    Formatter pass

--- src/N3Base/N3ShapeMgr.cpp
+++ src/N3Base/N3ShapeMgr.cpp
@@ -777,8 +777,7 @@ bool CN3ShapeMgr::CheckCollision(const __Vector3& vPos, // 충돌 위치
 
 	for (int i = 0; i < iSubCellCount; i++)
 	{
-		if (ppCells[i] == nullptr
-			|| ppCells[i]->nCCPolyCount <= 0)
+		if (ppCells[i] == nullptr || ppCells[i]->nCCPolyCount <= 0)
 			continue;
 
 		for (int j = 0; j < ppCells[i]->nCCPolyCount; j++)
