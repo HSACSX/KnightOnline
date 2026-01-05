f0858d14697a3e729507ae210603e531d484c560
twostars <i.am.twostars@gmail.com>
2026-01-05 21:05:29 +1100

    Just use spdlog here to avoid console contamination

--- src/N3Base/DebugUtils.cpp
+++ src/N3Base/DebugUtils.cpp
@@ -1,21 +1,17 @@
 ﻿#include "StdAfxBase.h"
+#include <spdlog/spdlog.h>
 
 void DebugStringToOutput(const std::string_view logMessage)
 {
 #ifdef _WIN32
 	constexpr std::string_view NewLine = "\r\n";
-#else
-	constexpr std::string_view NewLine = "\n";
-#endif
 
 	std::string outputMessage;
 	outputMessage.reserve(logMessage.size() + NewLine.size());
 	outputMessage  = logMessage;
 	outputMessage += NewLine;
-
-#ifdef WIN32
 	OutputDebugStringA(outputMessage.c_str());
-#else
-	printf("%s", outputMessage.c_str());
 #endif
+
+	spdlog::trace(logMessage);
 }
